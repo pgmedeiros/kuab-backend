7d914d671136e668cc21faccf4eefdb242f12b24
Pedro Medeiros <emaildopedromedeiros@gmail.com>
2024-01-14 02:57:47 -0300

    refatora repositorio

--- src/main/java/com/kuab/application/application/adapters/course/repository/JpaCourseRepository.java
+++ src/main/java/com/kuab/application/application/adapters/course/repository/JpaCourseRepository.java
@@ -1,13 +1,12 @@
-package com.kuab.application.application.adapters.out;
+package com.kuab.application.application.adapters.course.repository;
 
-import com.kuab.application.application.adapters.toEntity;
+import com.kuab.application.application.adapters.common.toEntity;
 import com.kuab.application.domain.course.model.Course;
 import com.kuab.application.domain.course.ports.out.CourseDatabasePort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.UUID;
 
 @Repository
 public class JpaCourseRepository implements CourseDatabasePort {
@@ -15,7 +14,11 @@ public class JpaCourseRepository implements CourseDatabasePort {
 
     @Autowired CourseSpringRepository springRepository;
     @Override
-    public List<Course> find(UUID id) {
-        return springRepository.findById(id).map(toEntity::convert).stream().toList();
+    public List<Course> find() {
+        return springRepository
+                .findAll()
+                .stream()
+                .map(toEntity::convert)
+                .toList();
     }
 }
